2553180b54d4fa37c4d70886329a75f526252183
Kio Gonzales <k.gonzales@student.saedubai.com>
2023-07-21 16:59:20 +0400

    Created FP view + Movement

---
+++ horror-game/Assets/Kio/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float movementSpeed;
+
+    public float groundDrag;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        rb.drag = groundDrag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyInput();
+        print(movementSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * movementSpeed * 10f, ForceMode.Force);
+    }
+}
