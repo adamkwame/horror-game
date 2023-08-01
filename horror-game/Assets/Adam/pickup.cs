b7d113c68b2342b937d72dd555ee51544ecb1c56
adam mekkaoui <a.mekkaoui@student.saedubai,com>
2023-08-01 11:20:25 +0400

    actual first build

--- horror-game/Assets/Adam/pickup.cs
+++ horror-game/Assets/Adam/pickup.cs
@@ -1,8 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
-using UnityEditor.Build;
 using UnityEngine;
 using TMPro;
 
